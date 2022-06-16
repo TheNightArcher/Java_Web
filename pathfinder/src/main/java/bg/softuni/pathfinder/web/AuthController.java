553183dcd29c9b436949d7c019a0a7569ce2a691
Galin Manov <thenightarchervclaymore@gmail.com>
2022-06-17 00:21:11 +0300

    Change HTML structure so make it more easy to wrok

--- pathfinder/src/main/java/bg/softuni/pathfinder/web/AuthController.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/web/AuthController.java
@@ -1,7 +1,14 @@
 package bg.softuni.pathfinder.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
+
+    @GetMapping()
+    public String Home(){
+
+        return "index";
+    }
 }
