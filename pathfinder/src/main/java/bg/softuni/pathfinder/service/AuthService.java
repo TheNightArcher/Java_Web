3760ccef8131bc244cf3be7ac339e4fe00e123b6
Galin Manov <thenightarchervclaymore@gmail.com>
2022-06-18 23:43:17 +0300

    init

--- pathfinder/src/main/java/bg/softuni/pathfinder/service/AuthService.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/service/AuthService.java
@@ -1,15 +1,23 @@
 package bg.softuni.pathfinder.service;
 
+import bg.softuni.pathfinder.model.UserEntity;
+import bg.softuni.pathfinder.model.dto.UserLoginDTO;
+import bg.softuni.pathfinder.model.dto.UserRegistrationDTO;
+import bg.softuni.pathfinder.model.enums.LevelEnum;
 import bg.softuni.pathfinder.repository.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
-    private UserRepository userRepository;
-    private ModelMapper modelMapper;
+    private final Logger LOGGER = LoggerFactory.getLogger(AuthService.class);
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
     public AuthService(UserRepository userRepository, ModelMapper modelMapper) {
@@ -18,7 +26,18 @@ public class AuthService {
     }
 
 
-    public void registration() {
+    public void registration(UserRegistrationDTO userRegistrationDTO) {
+
+        UserEntity user = modelMapper
+                .map(userRegistrationDTO, UserEntity.class);
+
+        user.setLevel(LevelEnum.BEGINNER);
+
+        userRepository.save(user);
+    }
+
+    public void login(UserLoginDTO userLoginDTO){
+
 
     }
 
--- pathfinder/src/main/java/bg/softuni/pathfinder/web/AuthController.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/web/AuthController.java
@@ -1,38 +1,71 @@
 package bg.softuni.pathfinder.web;
 
+import bg.softuni.pathfinder.model.dto.UserLoginDTO;
 import bg.softuni.pathfinder.model.dto.UserRegistrationDTO;
+import bg.softuni.pathfinder.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class AuthController {
 
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @ModelAttribute()
-    public UserRegistrationDTO userRegistrationDTO(){
+    public UserRegistrationDTO userRegistrationDTO() {
         return new UserRegistrationDTO();
     }
 
     @GetMapping("/home")
-    public String home(){
+    public String home() {
         return "index";
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 
+    @PostMapping("/login")
+    public String login(UserLoginDTO userLoginDTO) {
+
+        authService.login(userLoginDTO);
+        return "redirect:/";
+    }
+
     @GetMapping("/register")
-    public String register(){
+    public String register() {
         return "register";
     }
 
     @PostMapping("/register")
-    public String register(UserRegistrationDTO userRegistrationDTO){
+    public String register(@Valid UserRegistrationDTO userRegistrationDTO,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes) {
 
+        if (bindingResult.hasErrors()) {
 
-        return "redirect:/";
+            redirectAttributes.addFlashAttribute("userRegistrationDTO", userRegistrationDTO);
+
+            redirectAttributes
+                    .addFlashAttribute("org.springframework.validation.BindingResult.userRegistrationDTO", bindingResult);
+
+            return "redirect:/register";
+        }
+
+        authService.registration(userRegistrationDTO);
+        return "redirect:/login";
     }
 }
