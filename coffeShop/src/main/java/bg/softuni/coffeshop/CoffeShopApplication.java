28fc3e2debccab2c06cb400e3f1ffb42e85329a8
Galin Manov <thenightarchervclaymore@gmail.com>
2022-06-19 14:17:23 +0300

    Set configuration of this small project

---
+++ coffeShop/src/main/java/bg/softuni/coffeshop/CoffeShopApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.coffeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeShopApplication.class, args);
+    }
+
+}
