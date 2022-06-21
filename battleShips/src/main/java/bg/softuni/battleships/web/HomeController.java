f78eeccfa2bcb21a53ded7062b1fe3868bc16a2d
Galin Manov <thenightarchervclaymore@gmail.com>
2022-06-21 18:14:57 +0300

    Adding HTML and CSS

---
+++ battleShips/src/main/java/bg/softuni/battleships/web/HomeController.java
@@ -0,0 +1,15 @@
+package bg.softuni.battleships.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "index";
+    }
+
+
+}
