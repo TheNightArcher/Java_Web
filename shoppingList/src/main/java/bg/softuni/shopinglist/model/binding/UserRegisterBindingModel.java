bbd6ab957c7f84509d256441c5b00f65fa485010
Galin Manov <thenightarchervclaymore@gmail.com>
2022-06-23 18:36:32 +0300

    Init controllers

---
+++ shoppingList/src/main/java/bg/softuni/shopinglist/model/binding/UserRegisterBindingModel.java
@@ -0,0 +1,56 @@
+package bg.softuni.shopinglist.model.binding;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class UserRegisterBindingModel {
+
+    @NotNull
+    @Size(min = 3,max = 20)
+    private String username;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @NotNull
+    @Size(min = 3,max = 20)
+    private String password;
+
+    @NotNull
+    @Size(min = 3,max = 20)
+    private String confirmPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
---
+++ shoppingList/src/main/java/bg/softuni/shopinglist/web/UserController.java
@@ -0,0 +1,15 @@
+package bg.softuni.shopinglist.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+
+}
